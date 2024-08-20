3696daaebd14e21f7afe9e7a553536bb8a9e6ba7
Aashay <ashay9215@gmail.com>
2024-08-20 17:53:25 +0530

    Changed Gemini Default Prompt

--- gemini_client.py
+++ gemini_client.py
@@ -8,7 +8,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 def gemini_reply(command):
     chat = model.start_chat(
         history=[
-            {"role": "user", "parts": "You are a virtual assistant named John like Alexa. Answer any queries I ask in 100-150 words"},
+            {"role": "user", "parts": "You are a virtual assistant named John like Alexa. Answer any queries I ask in 100-150 words. Reply in a paragraph not in a list ever"},
             {"role": "model", "parts": "Great to meet you. What would you like to know?"},
         ]
     )
